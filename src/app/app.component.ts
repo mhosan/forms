06219e0af9c2cd94b541b7e067f8fbf4fb784377
mhosan <mhosan@gmail.com>
2023-06-19 19:11:16 -0300

    probando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  constructor(){
+    setInterval(()=>{
+      this.actualizarHora();
+    }, 1000);
+  }
+  actualizarHora() {
+  const reloj: HTMLElement | null = document.getElementById("reloj");
+  const fecha: Date = new Date();
+    let horas: number | string = fecha.getHours();
+    let minutos: number | string = fecha.getMinutes();
+    let segundos: number | string= fecha.getSeconds();
+    horas = (horas < 10) ? 0 + horas : horas;
+    minutos = (minutos < 10) ? 0 + minutos : minutos;
+    segundos = (segundos < 10) ? 0 + segundos : segundos;
+    const horaCompleta: string = horas + ":" + minutos + ":" + segundos;
+    if (reloj) { 
+    reloj.innerHTML = horaCompleta;
+  }
+  }
   
 }
+  
+
